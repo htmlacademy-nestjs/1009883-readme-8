68a9863bced92f75cb6b6bd10389c0fdd95fef5d
BlazyOne <BlazyOne@yandex.ru>
2024-12-05 16:27:13 +0500

    Adds some methods to AuthenticationService and BlogUserRepository

--- project/libs/user/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.service.ts
@@ -1,7 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { BlogUserRepository } from '@project/blog-user';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { BlogUserEntity, BlogUserRepository } from '@project/blog-user';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { AuthMessages } from './authentication.constant';
+import { LoginUserDto } from '../dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) {}
+
+  public async register(dto: CreateUserDto) {
+    const { email, name, avatar, password } = dto;
+
+    const blogUser = {
+      email,
+      name,
+      avatar,
+      passwordHash: '',
+    };
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AuthMessages.UserExists);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    this.blogUserRepository.save(userEntity);
+
+    return userEntity;
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new UnauthorizedException(AuthMessages.LoginFailed);
+    }
+
+    if (!(await existUser.comparePassword(password))) {
+      throw new UnauthorizedException(AuthMessages.LoginFailed);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    const user = await this.blogUserRepository.findById(id);
+
+    if (!user) {
+      throw new NotFoundException(AuthMessages.UserNotFound);
+    }
+
+    return user;
+  }
 }

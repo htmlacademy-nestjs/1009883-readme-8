1a63eb8b1875eab1c18a2499500db610403b2719
BlazyOne <BlazyOne@yandex.ru>
2024-12-25 19:21:25 +0500

    Adds validation to user app.

--- project/libs/user/authentication/src/dto/create-user.dto.ts
+++ project/libs/user/authentication/src/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, IsUrl, Length } from 'class-validator';
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User name',
     example: 'Keks',
   })
+  @IsString()
+  @Length(3, 50)
   public name: string;
 
   @ApiProperty({
@@ -18,11 +23,14 @@ export class CreateUserDto {
     example: '/images/user.png',
     required: false,
   })
+  @IsUrl()
   public avatar?: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456',
   })
+  @IsString()
+  @Length(6, 12)
   public password: string;
 }
